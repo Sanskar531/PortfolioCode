07453039077ee1744c40b5e86caf17ff6a22fbf5
Sanskar531 <sanskargauchanb4@gmail.com>
2022-01-29 18:49:15 +1100

    Images and layout fixed

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -1,5 +1,7 @@
 import { AnimatePresence, motion } from "framer-motion";
 import { useState } from "react";
+import brainTumorResults from "./assets/BrainTumorResults.png";
+import MCLR from "./assets/MCLR.png";
 
 function Project(props) {
   const [currProjIndex, setCurrProjInd] = useState(0);
@@ -17,7 +19,8 @@ function Project(props) {
 }
 
 function IndividualProjects(props) {
-  const [exitAnimation, setExitAnimation] = useState("");
+  const [enterExit, setEnterExit] = useState({ enter: "enterUp", exit: "" }); //Quickly re-renders to add the type of exit animation to complete.
+  console.log(enterExit);
   function transitionLength(i) {
     return {
       transition: {
@@ -26,22 +29,26 @@ function IndividualProjects(props) {
     };
   }
   const projectAnimations = {
-    hidden: {
+    enterDown: {
       y: "-100vh",
+      opacity: 0,
+    },
+    enterUp: {
+      y: "100vh",
       opacity: 0,
     },
     visible: {
       y: 0,
       opacity: 1,
-      ...transitionLength(1),
+      ...transitionLength(0.75),
     },
     exitDown: {
-      y: "100vh",
+      y: "-100vh",
       opacity: 0,
       ...transitionLength(1),
     },
     exitUp: {
-      y: "-100vh",
+      y: "100vh",
       opacity: 0,
       ...transitionLength(1),
     },
@@ -50,10 +57,12 @@ function IndividualProjects(props) {
     {
       name: "Brain Tumor Segmentation",
       desc: "Coded the U-net from scratch in python to segment tumors from normal Brain MRI images.Dealt with class imbalance since the tumor mask had a lot of 0’s but under-represented 1’s, which made the U-net segment to all 0’s that still yielded a good accuracy with MSE loss function but the image segmented nothing. Employed weighted cross entropy to give the class 1 in tumor class more precedence than 0’s in the tumor mask.",
+      img: brainTumorResults,
     },
     {
       name: "Multi-Class Regression Model",
       desc: "Implemented binary logistic regression model with sigmoid function that maps value between the range of 0 and 1. Used a threshold of 0.6 resulted in excellent accuracy. Hence, this was then used to make the model multi-class. Trained a number of logistic regressors, using one-vs-all concept, based on the number of classes existing on the dataset and, used the logistic regressor with the highest probability of 1. ",
+      img: MCLR,
     },
   ];
   const currRenderedProj = projects[props.ind];
@@ -61,29 +70,34 @@ function IndividualProjects(props) {
     <motion.div
       key={props.ind}
       variants={projectAnimations}
-      initial="hidden"
+      initial={enterExit.enter}
       animate="visible"
       className="projects"
-      exit={exitAnimation}
+      exit={enterExit.exit}
     >
       {props.ind !== 0 && (
         <button
           onClick={(e) => {
             e.preventDefault();
-            setExitAnimation("exitUp");
+            setEnterExit({ enter: "enterUp", exit: "exitUp" });
             props.handler(props.ind - 1);
           }}
         >
           Go Up
         </button>
       )}
-      <h1>{currRenderedProj.name}</h1>
-      <p>{currRenderedProj.desc}</p>
+      <div>
+        <img src={currRenderedProj.img} alt={currRenderedProj.name} />
+        <div>
+          <h1>{currRenderedProj.name}</h1>
+          <p>{currRenderedProj.desc}</p>
+        </div>
+      </div>
       {props.ind !== projects.length - 1 && (
         <button
           onClick={(e) => {
             e.preventDefault();
-            setExitAnimation("exitDown");
+            setEnterExit({ enter: "enterUp", exit: "exitDown" });
             props.handler(props.ind + 1);
           }}
         >
