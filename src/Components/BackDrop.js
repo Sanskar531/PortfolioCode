d24c04b399b08a40de3fa9c6c95c8b28beff1c4d
Sanskar531 <sanskargauchanb4@gmail.com>
2022-01-30 20:19:50 +1100

    Nav Bar styling and made responsive

--- src/Components/BackDrop.js
+++ src/Components/BackDrop.js
@@ -4,6 +4,11 @@ function BackDrop(props) {
   return (
     <motion.div
       className="backdrop"
+      initial={{
+        opacity: 0,
+      }}
+      animate={{ opacity: 1 }}
+      exit={{ opacity: 0 }}
       onClick={(e) => {
         e.preventDefault();
         props.exitHandler();
--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -44,22 +44,57 @@ function SocialLinks(props) {
   );
 }
 function NavMenu(props) {
+  const miniNavAnimations = {
+    initial: {
+      opacity: 0,
+      y: 5,
+    },
+    visible: (ind) => ({
+      opacity: 1,
+      y: 0,
+      transition: { delay: ind / 10 },
+    }),
+    exit: (ind) => ({
+      opacity: 0,
+      y: 5,
+      transition: { delay: ind / 15 },
+    }),
+  };
   function handler(e, redirect) {
     e.stopPropagation();
-    console.log(props.navigate);
+    props.exitHandler();
     props.navigate(redirect);
   }
   return (
     <motion.div className="navMenu">
-      <motion.a onClick={(e) => handler(e, "/")}>Home</motion.a>
-      <motion.a onClick={(e) => handler(e, "/projects")}>Projects</motion.a>
-      <motion.a onClick={(e) => handler(e, "/blog")}>Blog</motion.a>
-      <motion.a onClick={(e) => handler(e, "")}>FindMe</motion.a>
+      {props.navLinks.map((item, ind) => {
+        return (
+          <motion.a
+            custom={ind}
+            onClick={(e) => handler(e, item.redirect)}
+            variants={miniNavAnimations}
+            initial="initial"
+            animate="visible"
+            exit="exit"
+            key={ind}
+          >
+            {item.name}
+          </motion.a>
+        );
+      })}
     </motion.div>
   );
 }
 
 function Nav(props) {
+  const navLinks = [
+    {
+      name: "Home",
+      redirect: "/",
+    },
+    { name: "Projects", redirect: "/projects" },
+    { name: "Blog", redirect: "/blog" },
+  ];
   const navigate = useNavigate();
   const currDimension = useNavShow();
   const [navOpen, setNavOpen] = useState(false);
@@ -80,13 +115,15 @@ function Nav(props) {
             <motion.h1>Sanskar Gauchan</motion.h1>
           </motion.div>
           <motion.div className="NavBar">
-            <AnimatedLinks text="Home" href="/" handler={() => navigate("/")} />
-            <AnimatedLinks
-              text="Projects"
-              href="/Projects"
-              handler={() => navigate("/projects")}
-            />
-            <AnimatedLinks text="Blog" href="/Blog" />
+            {navLinks.map((item, ind) => {
+              return (
+                <AnimatedLinks
+                  key={ind}
+                  text={item.name}
+                  handler={() => navigate(item.redirect)}
+                />
+              );
+            })}
             <AnimatedLinks text="FindMe" handler={props.findHandler} />
             <div className="Links">
               <AnimatePresence>
@@ -105,23 +142,38 @@ function Nav(props) {
         </motion.nav>
       ) : (
         <motion.nav className="navMenuContainer">
-          <button
+          <motion.button
             onClick={(e) => {
               console.log("new");
               setNavOpen(!navOpen);
             }}
+            initial={{ borderRadius: "50%" }}
+            whileHover={{
+              scale: 1.2,
+              backgroundColor: "white",
+            }}
+            whileTap={{ scale: 0.8, backgroundColor: "#9575CD" }}
           >
             <span className="material-icons menuButton">menu</span>
-          </button>
+          </motion.button>
           <div>
             <h1>Sanskar Gauchan</h1>
           </div>
-          {navOpen && (
-            <BackDrop
-              children={<NavMenu navigate={navigate} />}
-              exitHandler={() => setNavOpen(!navOpen)}
-            />
-          )}
+          <AnimatePresence>
+            {navOpen && (
+              <BackDrop
+                key={navOpen}
+                children={
+                  <NavMenu
+                    navigate={navigate}
+                    exitHandler={() => setNavOpen(!navOpen)}
+                    navLinks={navLinks}
+                  />
+                }
+                exitHandler={() => setNavOpen(!navOpen)}
+              />
+            )}
+          </AnimatePresence>
         </motion.nav>
       )}
     </>
