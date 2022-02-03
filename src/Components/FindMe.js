0e5c83f0348eb70a8196957e95a9f5dd56c407cf
Sanskar531 <sanskargauchanb4@gmail.com>
2022-02-03 15:18:36 +1100

    Mobile button left to update findMe left to update

--- src/Components/FindMe.js
+++ src/Components/FindMe.js
@@ -1,6 +1,9 @@
 import { motion } from "framer-motion";
 
 function FindMe(props) {
+  function handler(e) {
+    e.preventDefault();
+  }
   const modalAnimations = {
     initial: {
       opacity: 0,
@@ -36,16 +39,34 @@ function FindMe(props) {
             props.exitHandler();
           }}
         >
-          X
+          <motion.span
+            initial={false}
+            whileHover={{ scale: 1.1 }}
+            whileTap={{ scale: 0.9 }}
+            className="material-icons closeButton"
+          >
+            close
+          </motion.span>
         </button>
       </div>
-      <form>
+      <form onSubmit={(e) => handler(e)}>
         <h4>Name</h4>
         <input type="text" name="Name"></input>
         <h4>Email</h4>
         <input type="email" name="E-mail"></input>
         <h4>Please mention the intent for contacting.</h4>
-        <textarea name="Description"></textarea>
+        <textarea name="Description" maxLength="5000"></textarea>
+        <div>
+          <motion.button
+            initial={false}
+            className="submitButton"
+            whileHover={{ scale: 1.1 }}
+            whileTap={{ scale: 0.9 }}
+            type="submit"
+          >
+            Send
+          </motion.button>
+        </div>
       </form>
     </motion.div>
   );
