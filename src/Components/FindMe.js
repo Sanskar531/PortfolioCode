6d7562644c2f60d8480b7872c33a1f26c52a4439
Sanskar531 <sanskargauchanb4@gmail.com>
2022-02-04 22:01:43 +1100

    Email service updated

--- src/Components/FindMe.js
+++ src/Components/FindMe.js
@@ -1,8 +1,32 @@
 import { motion } from "framer-motion";
+import constants from "../constants.js";
 
 function FindMe(props) {
   function handler(e) {
     e.preventDefault();
+    const body = {
+      name: e.target.Name.value,
+      message: e.target.Description.value,
+      email: e.target.Email.value,
+    };
+    const data = {
+      service_id: constants.service_id,
+      template_id: constants.template_id,
+      user_id: constants.user_id,
+      template_params: body,
+    };
+    fetch("https://api.emailjs.com/api/v1.0/email/send", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    })
+      .then(() => {
+        props.exitHandler();
+        alert("Message has been sent!");
+      })
+      .catch((err) => console.log(err));
   }
   const modalAnimations = {
     initial: {
@@ -32,7 +56,7 @@ function FindMe(props) {
       onClick={(e) => e.stopPropagation()}
     >
       <div>
-        <h4>You can find me on:</h4>
+        <h4>Send me a Message</h4>
         <button
           onClick={(e) => {
             e.preventDefault();
@@ -53,9 +77,9 @@ function FindMe(props) {
         <h4>Name</h4>
         <input type="text" name="Name"></input>
         <h4>Email</h4>
-        <input type="email" name="E-mail"></input>
+        <input type="email" name="Email"></input>
         <h4>Please mention the intent for contacting.</h4>
-        <textarea name="Description" maxLength="5000"></textarea>
+        <textarea name="Description"></textarea>
         <div>
           <motion.button
             initial={false}
